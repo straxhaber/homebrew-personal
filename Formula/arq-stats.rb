a698039f8ba082b74ae319860d05f48c489a3b00
Matthew Strax-Haber <web+git@straxhaber.com>
2026-02-16 16:19:11 -0800

    fix: correct arq-stats source revision

--- Formula/arq-stats.rb
+++ Formula/arq-stats.rb
@@ -1,7 +1,7 @@
 class ArqStats < Formula
   desc "Report most changed files from Arq backup logs"
   homepage "https://github.com/straxhaber/arq-stats"
-  url "https://github.com/straxhaber/arq-stats.git", using: :git, revision: "b824f099f5988d67f4918f83e57d45052ee14cf9"
+  url "https://github.com/straxhaber/arq-stats.git", using: :git, revision: "b824f097380cc74208a376f175efea91e71485a0"
   version "0.1.0"
 
   def install
