c5fa4f5a0cef49606cece70b13bcc27c6bf26362
Matthew Strax-Haber <web+git@straxhaber.com>
2026-02-16 16:15:01 -0800

    feat: add arq-stats formula

---
+++ Formula/arq-stats.rb
@@ -0,0 +1,14 @@
+class ArqStats < Formula
+  desc "Report most changed files from Arq backup logs"
+  homepage "https://github.com/straxhaber/arq-stats"
+  url "https://github.com/straxhaber/arq-stats.git", using: :git, revision: "b824f099f5988d67f4918f83e57d45052ee14cf9"
+  version "0.1.0"
+
+  def install
+    bin.install "arq_stats.py" => "arq-stats"
+  end
+
+  test do
+    assert_match "Top uploaded files from Arq backup logs.", shell_output("#{bin}/arq-stats --help")
+  end
+end
